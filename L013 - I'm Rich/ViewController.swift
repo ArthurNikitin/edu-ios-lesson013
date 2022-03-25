5588c8f26c5a0ce37556c2e357ad71281f0ff73b
ArthurNikitin <Arthur.V.Nikitin@gmail.com>
2022-03-25 11:29:57 +0300

    Create project
    I'm rich

---
+++ L013 - I'm Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  L013 - I'm Rich
+//
+//  Created by Arthur on 18.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
